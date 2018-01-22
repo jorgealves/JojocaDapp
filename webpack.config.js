20020a95fe5e34ce89fa445e3527aa1ed368d3e8
Jorge André Alves <jorgeandrealves@gmail.com>
2018-01-22 18:40:38 +0000

    asd

--- webpack.config.js
+++ webpack.config.js
@@ -6,25 +6,15 @@ module.exports = {
     entry: './jojoca_dapp/src/js/dapp.js',
     output: {
         path: path.resolve(__dirname, "jojoca_dapp/static"),
-        filename: 'js/dapp.js'
+        filename: 'js/app.js'
     },
     module: {
         rules: [
-            // {
-            //     test: /\.s[ac]ss$/,
-            //     use: ExtractTextPlugin.extract({
-            //         fallback: 'style-loader',
-            //         use: ['css-loader', 'sass-loader']
-            //     })
-            // },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
             }
-        ],
+        ]
     }
-    // plugins: [
-    //     new ExtractTextPlugin('css/styles.css')
-    // ]
 };
